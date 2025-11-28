1676f500d65c2e73b289a95becaac574bde8d971
Ehis Fidelis <meelisfidelis@gmail.com>
2025-11-28 07:22:13 +0100

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,14 @@
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-[var(--background-color)]">
+      <main className="flex min-h-fit w-fit max-w-3xl flex-col items-center justify-between py-10 px-9 bg-[var(--foreground-color)] shadow-lg rounded-xl">
+        <form>
+          <input type="email" name="email" placeholder="Email" required className="mb-4 w-full rounded-lg border border-gray-300 p-3 text-gray-900 dark:bg-gray-800 dark:text-white" />
+          <input type="password" name="password" placeholder="Password" required className="mb-4 w-full rounded-lg border border-gray-300 p-3 text-gray-900 dark:bg-gray-800 dark:text-white" />
+          <button type="submit" className="w-full rounded-lg bg-[var(--primary-color)] p-3 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+            Login
+          </button>          
+        </form>
        
       </main>
     </div>
